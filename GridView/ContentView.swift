c9c6eb435226859f22eddfd6e99975e8b1d2e61b
Steven Lipton <steve@makeapppie.com>
2020-09-22 06:09:58 -0500

    Initial Commit

---
+++ GridView/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  GridView
+//
+//  Created by Steven Lipton on 9/20/20.
+//
+// An exercise file for iOS Development Tips Weekly
+// A weekly course on LinkedIn Learning for iOS developers
+//  Season 12 (Q4 2020) video 08
+//  by Steven Lipton (C)2020, All rights reserved
+// Learn more at https://linkedin-learning.pxf.io/YxZgj
+//This Week:  LEarn the basics of collections views in SwiftUI
+//  For more code, go to http://bit.ly/AppPieGithub
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView{
+            VStack{
+                ForEach(MenuModel.menu){ item in
+                    VStack{
+                        Image("\(item.id)_100w")
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(5)
+                        HStack{
+                            Text("\(item.id)")
+                            Text(item.name)
+                        }.font(.caption)
+                    }
+                }
+            }.padding()
+       }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
